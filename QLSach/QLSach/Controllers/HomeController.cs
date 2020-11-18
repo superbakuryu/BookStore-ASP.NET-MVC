e025741f4f95c8377a68fc472996764ee5fa0665
Thuan <superbakuryu@gmail.com>
2020-11-18 13:31:42 +0700

    Them form login

--- QLSach/QLSach/Controllers/HomeController.cs
+++ QLSach/QLSach/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLSach.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,34 @@ namespace QLSach.Controllers
 {
     public class HomeController : Controller
     {
+        private BookShopEntities db = new BookShopEntities();
+
         public ActionResult Index()
+        {
+            if (Session["TaiKhoan"] != null)
+                return View();
+            return Redirect("~/Home/Login");
+        }
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var obj = db.tb_NguoiDung.Where(m=>m.TaiKhoan.Equals(email) && m.MatKhau.Equals(password)).FirstOrDefault();
+            if (obj == null)
+            {
+                ViewBag.ThongBao = "Không có tài khoản này";
+                return View();
+            }
+            else
+            {
+                Session["TaiKhoan"] = email;
+                return Redirect("~/Home/Index");
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
