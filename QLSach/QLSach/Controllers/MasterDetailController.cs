2f5d8d48d9eff8f0efd4a8810f08920d68ce1163
Thuan <superbakuryu@gmail.com>
2020-11-23 14:15:40 +0700

    Them bang Phieu Nhap

---
+++ QLSach/QLSach/Controllers/MasterDetailController.cs
@@ -0,0 +1,20 @@
+﻿using QLSach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLSach.Controllers
+{
+    public class MasterDetailController : Controller
+    {
+        BookShopEntities db = new BookShopEntities();
+        // GET: MasterDetail
+        public ActionResult Index()
+        {
+            List<tb_PhieuNhap> lists = db.tb_PhieuNhap.ToList();
+            return View(lists);
+        }
+    }
+}
