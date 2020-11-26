fa1a40bfad231f25d8f841e49ec28c58534803a7
Thuan <superbakuryu@gmail.com>
2020-11-27 01:24:45 +0700

    Auto complete textbox

--- QLSach/QLSach/Controllers/HomeController.cs
+++ QLSach/QLSach/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace QLSach.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Login(string email, string password)
+        public ActionResult Login(string email, string password)                                                    
         {
             var obj = db.tb_NguoiDung.Where(m=>m.TaiKhoan.Equals(email) && m.MatKhau.Equals(password)).FirstOrDefault();
             if (obj == null)
@@ -36,6 +36,22 @@ namespace QLSach.Controllers
                 Session["TaiKhoan"] = email;
                 return Redirect("~/Home/Index");
             }
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return Redirect("Login");
+        }
+
+        [HttpPost]
+        public JsonResult GetSearchValue(string search)
+        {
+            List<tb_Sach> allsearch = db.tb_Sach.Where(x => x.maSach.Contains(search)).Select(x => new tb_Sach
+            {
+                maSach = x.maSach,
+                tieuDe = x.tieuDe
+            }).ToList();
+            return new JsonResult { Data = allsearch, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
         public ActionResult About()
         {
